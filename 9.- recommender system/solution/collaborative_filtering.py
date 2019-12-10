2fce075ba310648531cdc1eb8e9bfb559e3f3e10
nik1168 <ngeisser32@gmail.com>
2019-12-10 14:58:17 +0100

    Add base for gradient check

--- 9.- recommender system/solution/collaborative_filtering.py
+++ 9.- recommender system/solution/collaborative_filtering.py
@@ -41,10 +41,92 @@ data_train = load_rating("u1.base").T
 data_test = load_rating("u1.test").T
 n_movies_train, n_users_train = data_train.shape
 n_movies_test, n_users_test = data_test.shape
+print("Finish")
 
 
 # create mask matrix
 # X: The rating matrix with size (n_movies,n_users)
 # Return: Binary mask matrix where 1 indicates there is a rating and 0 vice versa
 def create_mask(X):
-# YOUR CODE GOES HERE
+    B = X > 0
+    return B.astype(np.int)
+
+
+## Step 2: Implement functions to calculate cost and gradients
+# This function computes the cost value that we want to minimize
+# THETA: A matrix contains users' feature
+# X: A matrix contains movies' feature
+# Y: A matrix contains ground truth (n_movies x n_users)
+# _lambda: Regularization parameter
+# mask: The binary mask matrix
+def compute_cost(X, THETA, Y, _lambda, mask):
+    assert X.shape[1] == THETA.shape[1]
+    assert X.shape[0] == Y.shape[0]
+    assert THETA.shape[0] == Y.shape[1]
+    assert Y.shape == mask.shape
+    n_movies_train, n_users_train = X.shape
+    sum1 = 0
+    sum2 = 0
+    sum3 = 0
+    for i in range(n_movies_train):
+        for j in range(n_users_train):
+            if mask[i][j] == 1:
+                sum1 = sum1 + (np.matmul(THETA[j].T, X[i]) - Y[i][j]) ** 2
+    n_m = n_movies_train
+    n_u = n_users_train
+    for i in range(n_m):
+        for k in range(n_features):
+            sum2 = sum2 + (X[i][k]) ** 2
+
+    for i in range(n_u):
+        for k in range(n_features):
+            sum3 = sum3 + (X[i][k]) ** 2
+
+    return ((1 / 2) * sum1) + ((_lambda / 2) * sum2) + ((_lambda / 2) * sum3)
+
+
+# This function computes partial derivatives of the cost function with regards to movie and user features
+# THETA: A matrix contains users' feature
+# X: A matrix contains movies' feature
+# Y: A matrix contains ground truth (n_movies x n_users)
+# _lambda: Regularization parameter
+# mask: The binary mask matrix
+# return: a tuple (grad_X,grad_THETA)
+def compute_gradient(X, THETA, Y, _lambda, mask):
+    assert X.shape[1] == THETA.shape[1]
+    assert X.shape[0] == Y.shape[0]
+    assert THETA.shape[0] == Y.shape[1]
+    assert Y.shape == mask.shape
+    grad_X = X - np.dot(alpha, np.dot(np.dot((np.dot(THETA.T, X) - Y), THETA), mask) + np.dot(_lambda, X))
+    grad_THETA = THETA - np.dot(alpha,
+                                np.dot(np.dot((np.dot(THETA.T, X) - Y), X), mask) + np.dot(_lambda, THETA))
+    return (grad_X, grad_THETA)
+
+
+## Step 3: Training
+
+# %%
+
+n_features = 10
+MOVIE_FEATURES = 0.25 * np.random.randn(n_movies_train, n_features)
+USER_FEATURES = 0.25 * np.random.randn(n_users_train, n_features)
+_lambda = 0.01
+mask = create_mask(data_train)
+alpha = 0.001
+training_epochs = 150
+counter = 0
+
+while counter < training_epochs:
+    # Compute gradients
+    grad_X, grad_THETA = compute_gradient(MOVIE_FEATURES, USER_FEATURES, data_train, _lambda, mask)
+
+    # update parameters here
+    MOVIE_FEATURES = MOVIE_FEATURES - alpha * grad_X
+    USER_FEATURES = USER_FEATURES - alpha * grad_THETA
+
+    # compute cost function
+    cost = compute_cost(MOVIE_FEATURES, USER_FEATURES, data_train, _lambda, mask)
+
+    # increase counter
+    counter += 1
+    print("epoch:", counter, "cost: ", cost)
